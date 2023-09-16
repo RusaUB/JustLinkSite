058c7b4f8bb85799e047b6a647fe830cc5ab8217
Your Name <you@example.com>
2023-09-17 00:47:18 +0200

    DashBoard redirecting

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,12 +1,26 @@
+import { useEffect } from "react";
 import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 function DashBoard() {
-    const {currentUser} = useAuth();
-    return (
-        <div>
-            {JSON.stringify(currentUser).uid}
-        </div>
-    );
+  const { currentUser } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!currentUser) {
+      navigate("/login");
+    }
+  }, [currentUser]); // Add currentUser to the dependency array
+
+  return (
+    <div>
+      {currentUser ? (
+        <p>Welcome, {currentUser.displayName}!</p>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
 export default DashBoard;
